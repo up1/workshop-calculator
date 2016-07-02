dc0f11074aee6d826d3b4bbb9c4cda9cc89dfe02
Tanapruk <trusthaha@gmail.com>
2016-07-02 17:24:35 +0700

    merge

--- app/src/androidTest/java/com/workshop/calculator/ResultActivityTest.java
+++ app/src/androidTest/java/com/workshop/calculator/ResultActivityTest.java
@@ -1,5 +1,16 @@
 package com.workshop.calculator;
 
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +18,18 @@ import static org.junit.Assert.*;
  */
 public class ResultActivityTest {
 
+    public ViewInteraction displayResult;
+
+    @Rule
+    public ActivityTestRule activityTestRule = new ActivityTestRule(ResultActivity.class);
+
+    @BeforeClass
+    public void initialActivity() {
+        displayResult = onView(withId(R.id.display_result));
+    }
+
+    @Test
+    public void showdisplayResult() {
+        displayResult.check(matches(isDisplayed()));
+    }
 }
---
+++ app/src/test/java/com/workshop/calculator/DivideTest.java
@@ -0,0 +1,29 @@
+package com.workshop.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pm on 2/7/2559.
+ */
+public class DivideTest {
+    @Test
+    public void testDivide() {
+        assertEquals(new Divide().calculate(100,10),"10.00");
+        assertEquals(new Divide().calculate(123,10),"12.30");
+        assertEquals(new Divide().calculate(-123,10),"-12.30");
+        assertEquals(new Divide().calculate(1000,3),"333.33");
+        assertEquals(new Divide().calculate(1000,7),"142.86");
+        assertEquals(new Divide().calculate(1000,6),"166.67");
+    }
+    @Test
+    public void testDivideByZero() {
+        assertEquals(new Divide().calculate(100,0),"Infinity");
+    }
+    @Test
+    public void testZeroDivide() {
+        assertEquals(new Divide().calculate(0,100),"0.00");
+    }
+}
