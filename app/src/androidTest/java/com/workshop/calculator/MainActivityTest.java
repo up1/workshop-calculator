3a206098ad659125e6d9ee699ed938a0e1119d85
Tanapruk <trusthaha@gmail.com>
2016-07-02 17:14:50 +0700

    Merge branch 'master' of https://github.com/up1/workshop-calculator
    update layout

--- app/src/androidTest/java/com/workshop/calculator/MainActivityTest.java
+++ app/src/androidTest/java/com/workshop/calculator/MainActivityTest.java
@@ -1,5 +1,11 @@
 package com.workshop.calculator;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static org.junit.Assert.*;
 
 /**
@@ -8,4 +14,18 @@ import static org.junit.Assert.*;
 
 public class MainActivityTest {
 
+    @BeforeClass
+    public void initialActivity() {
+
+    }
+
+    @Test
+    public void showEditTextNumber1(){
+        onView(R.id.edittext_number1).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showEditTextNumber2(){
+        onView(R.id.edittext_number2).check(matches(isDisplayed()));
+    }
 }
