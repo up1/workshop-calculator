2c2a45ec5b11770eef3e8f01586fe4e2c29b601e
Tepong <Te_pong@hotmail.com>
2016-07-02 17:19:38 +0700

    Get Result from intent to show and condition err when more than 1 million

--- app/src/main/java/com/workshop/calculator/ResultActivity.java
+++ app/src/main/java/com/workshop/calculator/ResultActivity.java
@@ -1,13 +1,25 @@
 package com.workshop.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String result = intent.getStringExtra("display_result");
+        textView = (TextView) findViewById(R.id.display_result);
+        if(Float.parseFloat(result) >= 1000000.00){
+            textView.setText("***ERROR***");
+        }else{
+            textView.setText(result);
+        }
     }
 }
