d31656108067c4b1789363080688617e60c13306
um.anusorn@gmail.com <um.anusorn@gmail.com>
2016-07-05 23:43:00 +0700

    bind result to textView

--- app/src/main/java/com/workshop/calculator/MainActivity.java
+++ app/src/main/java/com/workshop/calculator/MainActivity.java
@@ -6,32 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.workshop.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     private String currentOperator;
-
     static final String DISPLAY_RESULT = "display_result";
-
-    ActivityMainBinding activityMainBinding;
+    private ActivityMainBinding binder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binder = DataBindingUtil.setContentView(this, R.layout.activity_main);
         initInstance();
     }
 
     private void initInstance() {
-        activityMainBinding.buttonOk.setOnClickListener(this);
-        activityMainBinding.operatorPlus.setOnClickListener(this);
-        activityMainBinding.operatorMinus.setOnClickListener(this);
-        activityMainBinding.operatorDivide.setOnClickListener(this);
-        activityMainBinding.operatorMultiply.setOnClickListener(this);
+        binder.buttonOk.setOnClickListener(this);
+        binder.operatorPlus.setOnClickListener(this);
+        binder.operatorMinus.setOnClickListener(this);
+        binder.operatorDivide.setOnClickListener(this);
+        binder.operatorMultiply.setOnClickListener(this);
     }
 
 
@@ -40,11 +35,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private double getLeftNumber() {
-        return Double.valueOf(numberize(activityMainBinding.edittextNumber1.getText().toString()));
+        return Double.valueOf(numberize(binder.edittextNumber1.getText().toString()));
     }
 
     private double getRightNumber() {
-        return Double.valueOf(numberize(activityMainBinding.edittextNumber2.getText().toString()));
+        return Double.valueOf(numberize(binder.edittextNumber2.getText().toString()));
     }
 
     private String numberize(String string) {
@@ -91,18 +86,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v == activityMainBinding.buttonOk) {
+        if (v == binder.buttonOk) {
             if (getCurrentOperator() != null) {
                 operate();
             }
-
-        } else if (v == activityMainBinding.operatorPlus) {
+        } else if (v == binder.operatorPlus) {
             setCurrentOperator("+");
-        } else if (v == activityMainBinding.operatorMinus) {
+        } else if (v == binder.operatorMinus) {
             setCurrentOperator("-");
-        } else if (v == activityMainBinding.operatorMultiply) {
+        } else if (v == binder.operatorMultiply) {
             setCurrentOperator("*");
-        } else if (v == activityMainBinding.operatorDivide) {
+        } else if (v == binder.operatorDivide) {
             setCurrentOperator("/");
         }
 
--- app/src/main/java/com/workshop/calculator/ResultActivity.java
+++ app/src/main/java/com/workshop/calculator/ResultActivity.java
@@ -4,22 +4,19 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.workshop.calculator.databinding.ActivityResultBinding;
 
 public class ResultActivity extends AppCompatActivity {
-
-    private TextView textView;
-ActivityResultBinding activityResultBinding;
+    private static final String DISPLAY_RESULT = "display_result";
+    private ActivityResultBinding binder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        activityResultBinding= DataBindingUtil.setContentView(this,R.layout.activity_result);
-
+        binder = DataBindingUtil.setContentView(this,R.layout.activity_result);
         Intent intent = getIntent();
-        String result = intent.getStringExtra("display_result");
-        activityResultBinding.displayResult.setText(result);
+        CalculatorValue result=new CalculatorValue(intent.getStringExtra(DISPLAY_RESULT));
+        binder.setCalculatorValue(result);
     }
 }
